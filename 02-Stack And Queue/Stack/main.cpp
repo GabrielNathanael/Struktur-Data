f2b45078c6f864d8e5e0650663b5b6777329f5d6
GabrielNathanael <118794975+GabrielNathanael@users.noreply.github.com>
2023-04-25 11:55:28 +0800

    Add files via upload

---
+++ 02-Stack And Queue/Stack/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "stack.h"
+
+using namespace std;
+
+int main() {
+    Stack myStack;
+    myStack.push(3);
+    myStack.push(5);
+    myStack.push(7);
+    myStack.printElement();
+
+
+    return 0;
+}
---
+++ 02-Stack And Queue/Stack/stack.h
@@ -0,0 +1,18 @@
+class Node {
+public:
+    int data;
+    Node* next;
+};
+
+class Stack {
+private:
+    Node* top;
+
+public:
+    Stack();
+    void push(int x);
+    bool isEmpty();
+    int pop();
+    void printStack();
+    void printElement();
+};
